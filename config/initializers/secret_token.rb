ac98aaad355e4d91938a166d92135532d0425b90
Thomas Landspurg <thomas.landspurg@gmail.com>
2014-05-10 18:28:56 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenSensor::Application.config.secret_token = 'e919ab5dc7622e97caaa9dbe6301eed96a0dd7abe9991a267c183ad8ba6a8371803c50b1869f123ae575ed2b27775767b099b87d7d223dee7b1def39274dc6b2'
