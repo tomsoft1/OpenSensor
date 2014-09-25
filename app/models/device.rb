a2e7f3a4cab65eb9215d8a17cc925396b7b086dc
Thomas Landspurg <thomas.landspurg@gmail.com>
2014-09-25 15:33:57 +0200

    find_by_name_or_create was not taking in account device

--- app/models/device.rb
+++ app/models/device.rb
@@ -21,7 +21,7 @@ class Device
 			sensor=Sensor.find_by_name_or_create key,self
 			if value.class==Hash && value["lat"]!=nil then sensor.set(:type,"Position") end
 			measure=sensor.add_measure(value,time )
-			f.write("Adding: #{measure.to_s}\n")
+			f.write("Adding: #{measure.inspetc}\n")
 			sensors<<sensor
 		end
 		# check if we get signaldata
@@ -29,7 +29,7 @@ class Device
 			sensor=Sensor.find_by_name_or_create("signal",self)
 			measure=sensor.add_measure(params["signal"].to_f,time )
 			sensors<<sensor
-			f.write("Adding: #{measure.inspect}\n")
+			f.write("Adding Signal: #{measure.inspect}\n")
 		end
 		f.close
 		Sensor.check_and_update sensors
