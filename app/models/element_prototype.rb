e928080f751fa6464df699de28505961c2950a36
Thomas Landspurg <thomas.landspurg@gmail.com>
2016-12-14 10:46:36 +0100

    Minor update

--- app/models/element_prototype.rb
+++ app/models/element_prototype.rb
@@ -1,28 +1,31 @@
 class ElementPrototype
   include Mongoid::Document
   field :name, type: String
-  field :icon, type:String
-  field :description, type:String 
+  field :icon, type: String
+  field :description, type: String
   embeds_many :parameter_def
   has_many :widgets
 end
 
 class ParameterDef
-    include Mongoid::Document
-	field :name, type:String
-	field :mandatory, type:Boolean,:default=>false
-	field :default  , type:Object 
-	field :description, type:String
-	embedded_in :ElementPrototype
-	def name_param	
-		res=name.downcase
-		if res=="type" then res="param_"+res end
-		res
-	end
+  include Mongoid::Document
+  field :name, type: String
+  field :mandatory, type: Boolean, :default => false
+  field :default, type: Object
+  field :description, type: String
+  embedded_in :ElementPrototype
+
+  def name_param
+    res=name.downcase
+    if res=="type" then
+      res="param_"+res
+    end
+    res
+  end
 end
 
 class ParameterString < ParameterDef
-	
+
 end
 
 class ParameterBoolean < ParameterDef
@@ -35,12 +38,12 @@ class ParameterInteger < ParameterDef
 end
 
 class ParameterEnum < ParameterDef
-	field :values, type:Array
+  field :values, type: Array
 end
 
 class ParameterSensor < ParameterDef
-	field  :can_be_empty, type:Boolean,:default=>false
-	has_one :sensor
+  field :can_be_empty, type: Boolean, :default => false
+  has_one :sensor
 end
 
 
